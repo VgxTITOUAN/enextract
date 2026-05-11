969bd3e18dbe144fd0a88538da462de271e47e21
Titouan Vigouroux <titouan.vigouroux29@gmail.com>
2026-05-11 15:28:24 +0200

    feat: fix logout ( confirm logout )

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -4,6 +4,7 @@ export default function LogoutButton() {
   return (
     <button
       onClick={async () => {
+        if (!confirm('Voulez-vous vous déconnecter ?')) return;
         await fetch('/api/auth/logout', { method: 'POST' });
         window.location.href = '/login';
       }}
